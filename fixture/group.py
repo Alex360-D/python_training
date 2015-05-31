da5493c359a98d8afc002f3cb141ea459e069b4f
Alex360-D <alex360.dors@yandex.ru>
2015-05-31 21:04:13 +0300

    Оптимизирован переход между страницами

--- fixture/group.py
+++ fixture/group.py
@@ -7,7 +7,9 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        # Проверяем страницу
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def setvalue(self, value, text):
         wd = self.app.wd
