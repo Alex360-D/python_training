6b3a222d296ce2debd6b3dcddcf93f1acc291c3a
Alex360-D <alex360.dors@yandex.ru>
2015-07-08 17:58:54 +0300

    21-con

--- test/test_contacts.py
+++ test/test_contacts.py
@@ -1,34 +1,21 @@
 # -*- coding: utf-8 -*-
 
 import re
-from random import randrange
 from model.contact import Contact
 
-def test_contacts_on_home_page(app):
-    if app.contact.count() == 0:
+def test_contacts_on_home_page(app, db):
+    if len(db.get_contact_list()) == 0:
         app.contact.add(Contact(firstname="test_contact"))
-    index = randrange(app.contact.count())
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    list_contacts_from_home_page = app.contact.get_contact_list()
+    list_contacts_db = db.get_contact_list()
+    assert sorted(list_contacts_from_home_page, key=Contact.id_or_max) == sorted(list_contacts_db, key=Contact.id_or_max)
 
-# Сравнения view и edit
-'''
-def test_phones_on_contact_view_page(app):
-    if app.contact.count() == 0:
-        app.contact.add(Contact(firstname="test_contact"))
-    index = randrange(app.contact.count())
-    contact_from_view_page = app.contact.get_contact_from_view_page(index)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_view_page.home == contact_from_edit_page.home
-    assert contact_from_view_page.mobile == contact_from_edit_page.mobile
-    assert contact_from_view_page.work == contact_from_edit_page.work
-    assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
-'''
+
+    # assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+    # assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+    # assert contact_from_home_page.address == contact_from_edit_page.address
+    # assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+    # assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 def clear(s):
     return re.sub("[() -]", "", s)
