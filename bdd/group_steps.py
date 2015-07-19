a9ac6557e1b81fd9bd48df4af5944da33e65155c
Alex360-D <alex360.dors@yandex.ru>
2015-07-19 14:02:15 +0300

    10.1 - bdd (mod group)

--- bdd/group_steps.py
+++ bdd/group_steps.py
@@ -4,6 +4,7 @@ from pytest_bdd import given, when, then
 from model.group import Group
 import random
 
+# Add
 @given('a group list')
 def group_list(db):
     return db.get_group_list()
@@ -17,12 +18,16 @@ def add_new_group(app, new_group):
     app.group.create(new_group)
 
 @then('the new group list is equal to the old list with the added group')
-def verify_group_added(db, group_list, new_group):
+def verify_group_added(db, group_list, new_group, app, check_ui):
     old_groups = group_list
     new_groups = db.get_group_list()
     old_groups.append(new_group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
+
+# Delete
 
 @given('a non-empty group list')
 def non_empty_group_list(db, app):
@@ -45,5 +50,38 @@ def verify_group_deleted(db, non_empty_group_list, random_group, app, check_ui):
     assert len(old_groups) - 1 == len(new_groups)
     old_groups.remove(random_group)
     assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+
+# Modify
+
+# @given('a non-empty group list')
+# def non_empty_group_list(db, app):
+#     if len(db.get_group_list()) == 0:
+#         app.group.create(Group(name="test group"))
+#     return db.get_group_list()
+
+@given('a random group from the list')
+def index_randrange_group(non_empty_group_list):
+    return random.randrange(len(non_empty_group_list))
+
+@given('a group with <name>, <header> and <footer>')
+def mod_group(name, header, footer):
+    return Group(name=name, header=header, footer=footer)
+
+@when('I modify the group in the list')
+def modify_group(app, non_empty_group_list, index_randrange_group, mod_group):
+    old_groups = non_empty_group_list
+    index = index_randrange_group
+    mod_group.id = old_groups[index].id
+    app.group.modify_group_by_id(mod_group.id, mod_group)
+
+@then('the new group list is equal to the old list with the modified group')
+def verify_group_modified(app, db, non_empty_group_list, index_randrange_group, mod_group, check_ui):
+    old_groups = non_empty_group_list
+    index = index_randrange_group
+    new_groups = db.get_group_list()
+    old_groups[index] = mod_group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
