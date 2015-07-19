976e0c408ebd02940d5f7b9f31ec98d3abc6f9be
Alex360-D <alex360.dors@yandex.ru>
2015-07-20 00:28:54 +0300

    10.3 - Allure (mod group)

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -2,18 +2,22 @@
 
 from model.group import Group
 from random import randrange
-
+import pytest
 
 def test_modify_some_group_name(app, db, check_ui):
-    if len(db.get_group_list()) == 0:
-        app.group.create(Group(name="test_group"))
-    old_groups = db.get_group_list()
+    with pytest.allure.step('Given a group list'):
+        if len(db.get_group_list()) == 0:
+            app.group.create(Group(name="test_group"))
+        old_groups = db.get_group_list()
     index = randrange(len(old_groups))
     group = Group(name="aaaa_mod")
     group.id = old_groups[index].id
-    app.group.modify_group_by_id(group.id, group)
-    new_groups = db.get_group_list()
-    old_groups[index] = group
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    with pytest.allure.step('When I modify the group %s in the list' % group):
+        app.group.modify_group_by_id(group.id, group)
+    with pytest.allure.step('Then the new group list is equal to the old list with the modified group'):
+        new_groups = db.get_group_list()
+        old_groups[index] = group
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     if check_ui:
-        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+        with pytest.allure.step('Also check UI'):
+            assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
